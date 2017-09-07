6ac17031e300255fad7f19cd9e5f43cabbd122d6
Matthew Klein <mklein994@gmail.com>
2017-09-07 16:27:03 -0500

    Try a quick query to test things out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate postgres;
+
+use postgres::{Connection, TlsMode};
+
+struct Employee {
+    fname: String,
+    //minit: String,
+    //lname: String,
+}
+
 fn main() {
+    let conn = Connection::connect("postgresql://u0_a88@localhost:5432/company", TlsMode::None).unwrap();
+
+    for row in &conn.query("SELECT fname from employee;", &[]).unwrap() {
+        let employee = Employee {
+            fname: row.get(0),
+            //minit: row.get(1),
+            //lname: row.get(2),
+        };
+
+        println!("found Employee: {}", employee.fname);
+    }
+
     println!("Hello, world!");
 }
